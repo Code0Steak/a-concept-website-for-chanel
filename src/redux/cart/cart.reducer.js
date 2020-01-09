8f8383f8e1e83c1198775ab1bdf0d237e64d7166
Ameya Keskar <ameyakeskar1999@gmail.com>
2020-01-09 16:23:51 +0530

    added qt field

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,7 @@
 import CartActionTypes from './cart.types'
 
+import {addItemToCart} from '../../components/cart/cart.utils'
+
 const INITIAL_STATE = {
     hidden: true,
     cartItems: []
@@ -10,11 +12,11 @@ const cartReducer = (state = INITIAL_STATE,action) => {
     switch(action.type){
         case CartActionTypes.TOGGLE_CART_HIDDEN: return {
             ...state, hidden: !state.hidden
-        }
+        };
         case CartActionTypes.ADD_ITEM: return {
-            ...state, cartItems: [...state.cartItems, action.payload]
-        }
-        default: return state
+            ...state, cartItems: addItemToCart(state.cartItems, action.payload)
+        };
+        default: return state;
 
     }
 
