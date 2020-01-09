faf2a04cdcbd0a736a5cfff97b7031e3ddab4d1b
Ameya Keskar <ameyakeskar1999@gmail.com>
2020-01-09 20:43:41 +0530

    added CHANEL data again!

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,14 +6,13 @@ import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
 const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
-      <Logo className='logo' />
+      CHANEL
     </Link>
     <div className='options'>
       <Link className='option' to='/shop'>
--- src/pages/shop/shop.data.js
+++ src/pages/shop/shop.data.js
@@ -1,249 +1,159 @@
-const SHOP_DATA = [
+const SHOP_DATA =  [
   {
     id: 1,
-    title: 'Hats',
-    routeName: 'hats',
+    title: 'Fine Jewellery',
+    routeName: 'FineJewellery',
     items: [
       {
         id: 1,
-        name: 'Brown Brim',
-        imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
-        price: 25
+        name: 'Coco Crush Ring',
+        imageUrl: 'https://www.chanel.com/wfj/product/medias/coco-crush-ring/J10574-default-0-1540-grey-nocrop-1476739236193.jpg',
+        price: 2500
       },
       {
         id: 2,
-        name: 'Blue Beanie',
-        imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
-        price: 18
+        name: 'Coco Crush Bracelet',
+        imageUrl: 'https://www.chanel.com/wfj/product/medias/coco-crush-bracelet/J11162-default-0-1540-grey-nocrop-1490266141067.jpg',
+        price: 2600
       },
       {
         id: 3,
-        name: 'Brown Cowboy',
-        imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
-        price: 35
+        name: 'Ultra Ring',
+        imageUrl: 'https://www.chanel.com/wfj/product/medias/ultra-ring/J3092-default-0-1540-grey-nocrop-1476739226803.jpg',
+        price: 1370
       },
       {
         id: 4,
-        name: 'Grey Brim',
-        imageUrl: 'https://i.ibb.co/RjBLWxB/grey-brim.png',
+        name: 'Ultra Neclace',
+        imageUrl: 'https://www.chanel.com/wfj/product/medias/ultra-necklace/J3171-alternative-0-1540-grey-nocrop-1476739237490.jpg',
         price: 25
       },
-      {
-        id: 5,
-        name: 'Green Beanie',
-        imageUrl: 'https://i.ibb.co/YTjW3vF/green-beanie.png',
-        price: 18
-      },
-      {
-        id: 6,
-        name: 'Palm Tree Cap',
-        imageUrl: 'https://i.ibb.co/rKBDvJX/palm-tree-cap.png',
-        price: 14
-      },
-      {
-        id: 7,
-        name: 'Red Beanie',
-        imageUrl: 'https://i.ibb.co/bLB646Z/red-beanie.png',
-        price: 18
-      },
-      {
-        id: 8,
-        name: 'Wolf Cap',
-        imageUrl: 'https://i.ibb.co/1f2nWMM/wolf-cap.png',
-        price: 14
-      },
-      {
-        id: 9,
-        name: 'Blue Snapback',
-        imageUrl: 'https://i.ibb.co/X2VJP2W/blue-snapback.png',
-        price: 16
-      }
+
     ]
   },
   {
     id: 2,
-    title: 'Sneakers',
-    routeName: 'sneakers',
+    title: 'Watches',
+    routeName: 'Watches',
     items: [
       {
-        id: 10,
-        name: 'Adidas NMD',
-        imageUrl: 'https://i.ibb.co/0s3pdnc/adidas-nmd.png',
-        price: 220
+        id: 1,
+        name: 'J12 Black',
+        imageUrl: 'https://www.chanel.com/wfj/product/medias/j12-watch/H5697-default-0-1540-grey-nocrop-1552544306527.jpg',
+        price: 4675
       },
       {
-        id: 11,
-        name: 'Adidas Yeezy',
-        imageUrl: 'https://i.ibb.co/dJbG1cT/yeezy.png',
+        id: 2,
+        name: 'J12 White',
+        imageUrl: 'https://www.chanel.com/wfj/product/medias/j12-watch/H5705-default-0-1540-grey-nocrop-1562302941760.jpg',
         price: 280
       },
       {
-        id: 12,
-        name: 'Black Converse',
-        imageUrl: 'https://i.ibb.co/bPmVXyP/black-converse.png',
+        id: 3,
+        name: 'J12 Jewellery',
+        imageUrl: 'https://www.chanel.com/wfj/product/medias/j12-jewellery-watch/H3384-default-0-1540-grey-nocrop-1476739212803.jpg',
         price: 110
       },
       {
-        id: 13,
-        name: 'Nike White AirForce',
-        imageUrl: 'https://i.ibb.co/1RcFPk0/white-nike-high-tops.png',
+        id: 4,
+        name: 'Code Coco',
+        imageUrl: 'https://www.chanel.com/wfj/product/medias/code-coco-watch/H5145-default-0-1540-grey-nocrop-1517810682230.jpg',
         price: 160
-      },
-      {
-        id: 14,
-        name: 'Nike Red High Tops',
-        imageUrl: 'https://i.ibb.co/QcvzydB/nikes-red.png',
-        price: 160
-      },
-      {
-        id: 15,
-        name: 'Nike Brown High Tops',
-        imageUrl: 'https://i.ibb.co/fMTV342/nike-brown.png',
-        price: 160
-      },
-      {
-        id: 16,
-        name: 'Air Jordan Limited',
-        imageUrl: 'https://i.ibb.co/w4k6Ws9/nike-funky.png',
-        price: 190
-      },
-      {
-        id: 17,
-        name: 'Timberlands',
-        imageUrl: 'https://i.ibb.co/Mhh6wBg/timberlands.png',
-        price: 200
       }
     ]
   },
   {
     id: 3,
-    title: 'Jackets',
-    routeName: 'jackets',
+    title: 'Skincare',
+    routeName: 'Skincare',
     items: [
       {
-        id: 18,
-        name: 'Black Jean Shearling',
-        imageUrl: 'https://i.ibb.co/XzcwL5s/black-shearling.png',
+        id: 1,
+        name: 'Hand Cream',
+        imageUrl: 'https://www.chanel.com/fnbv3/image/full/chanel__com_type1/skus_full/le-lift-the-smoothing-even-toning-and-replenishing-hand-cream-bottle-50ml.3145891416404.jpg',
         price: 125
       },
       {
-        id: 19,
-        name: 'Blue Jean Jacket',
-        imageUrl: 'https://i.ibb.co/mJS6vz0/blue-jean-jacket.png',
+        id: 2,
+        name: 'CC Cream',
+        imageUrl: 'https://www.chanel.com/fnbv3/image/full/chanel__com_type1/skus_full/cc-cream-super-active-complete-correction-spf-50-10-beige-tube-30ml.3145891405552.jpg',
         price: 90
       },
       {
-        id: 20,
-        name: 'Grey Jean Jacket',
-        imageUrl: 'https://i.ibb.co/N71k1ML/grey-jean-jacket.png',
+        id: 3,
+        name: 'Sublimage',
+        imageUrl: 'https://www.chanel.com/fnbv3/image/512/chanel__com/skus_relative/sublimage-le-teint-ultimate-radiance-generating-cream-foundation-12-beige-rose-30g.3145891466102.jpg',
         price: 90
       },
       {
-        id: 21,
-        name: 'Brown Shearling',
-        imageUrl: 'https://i.ibb.co/s96FpdP/brown-shearling.png',
+        id: 4,
+        name: 'Special Edition',
+        imageUrl: 'https://prd-v3-i.chanel.com/content/dam/fnb/core/onlyat/pushlanding/UK-without-CTA.adaptive.1600.jpg',
         price: 165
-      },
-      {
-        id: 22,
-        name: 'Tan Trench',
-        imageUrl: 'https://i.ibb.co/M6hHc3F/brown-trench.png',
-        price: 185
       }
     ]
   },
   {
     id: 4,
-    title: 'Womens',
-    routeName: 'womens',
+    title: 'Fragrance',
+    routeName: 'Fragrance',
     items: [
       {
-        id: 23,
-        name: 'Blue Tanktop',
-        imageUrl: 'https://i.ibb.co/7CQVJNm/blue-tank.png',
-        price: 25
+        id: 1,
+        name: 'Coco Intense',
+        imageUrl: 'https://www.chanel.com/fnbv3/image/full/chanel__com_type1/skus_full/coco-mademoiselle-eau-de-parfum-intense-spray-100ml.3145891166606.jpg',
+        price: 118
       },
       {
-        id: 24,
-        name: 'Floral Blouse',
-        imageUrl: 'https://i.ibb.co/4W2DGKm/floral-blouse.png',
-        price: 20
+        id: 2,
+        name: 'Coco Mademoiselle',
+        imageUrl: 'https://www.chanel.com/fnbv3/image/full/chanel__com_type1/skus_full/coco-mademoiselle-moisturising-body-lotion-200ml.3145891169454.jpg',
+        price: 200
       },
       {
-        id: 25,
-        name: 'Floral Dress',
-        imageUrl: 'https://i.ibb.co/KV18Ysr/floral-skirt.png',
-        price: 80
+        id: 3,
+        name: 'Bleu de Chanel',
+        imageUrl: 'https://www.chanel.com/fnbv3/image/full/chanel__com_type1/skus_full/bleu-de-chanel-parfum-spray-100ml.3145891071801.jpg',
+        price: 810
       },
       {
-        id: 26,
-        name: 'Red Dots Dress',
-        imageUrl: 'https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png',
-        price: 80
-      },
-      {
-        id: 27,
-        name: 'Striped Sweater',
-        imageUrl: 'https://i.ibb.co/KmSkMbH/striped-sweater.png',
-        price: 45
-      },
-      {
-        id: 28,
-        name: 'Yellow Track Suit',
-        imageUrl: 'https://i.ibb.co/v1cvwNf/yellow-track-suit.png',
-        price: 135
-      },
-      {
-        id: 29,
-        name: 'White Blouse',
-        imageUrl: 'https://i.ibb.co/qBcrsJg/white-vest.png',
-        price: 20
+        id: 4,
+        name: 'N deg5',
+        imageUrl: 'https://www.chanel.com/fnbv3/image/512/chanel__com/skus_relative/n_5-n_5-fragments-dor-250ml.3145891058185.jpg',
+        price: 180
       }
     ]
   },
   {
     id: 5,
-    title: 'Mens',
-    routeName: 'mens',
+    title: 'Sunglasses',
+    routeName: 'Sunglasses',
     items: [
       {
-        id: 30,
-        name: 'Camo Down Vest',
-        imageUrl: 'https://i.ibb.co/xJS0T3Y/camo-vest.png',
+        id: 1,
+        name: 'Cat Eye',
+        imageUrl: 'https://www.chanel.com/en_GB/fashion/sunglasses/pub/media/catalog/product/cache/small_image/700x560/799896e5c6c37e11608b9f8e1d047d15/f/a/fall19-a71326-x01060-l9514-4255-c395-s4-pdt_view_1.jpg',
         price: 325
       },
       {
-        id: 31,
-        name: 'Floral T-shirt',
-        imageUrl: 'https://i.ibb.co/qMQ75QZ/floral-shirt.png',
-        price: 20
+        id: 2,
+        name: 'Oval',
+        imageUrl: 'https://www.chanel.com/en_GB/fashion/sunglasses/pub/media/catalog/product/cache/small_image/700x560/799896e5c6c37e11608b9f8e1d047d15/f/a/fall19-a71321-x02016-s7148-5411-c714-83-pdt_view_1.jpg',
+        price: 200
       },
       {
-        id: 32,
-        name: 'Black & White Longsleeve',
-        imageUrl: 'https://i.ibb.co/55z32tw/long-sleeve.png',
+        id: 3,
+        name: 'Pilot',
+        imageUrl: 'https://www.chanel.com/en_GB/fashion/sunglasses/pub/media/catalog/product/cache/image/700x560/af097278c5db4767b0fe9bb92fe21690/s/p/spring20-a71354-x02569-s6612-5423b-1661-b8-pdt_view_1.jpg',
         price: 25
       },
       {
-        id: 33,
-        name: 'Pink T-shirt',
-        imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
-        price: 25
-      },
-      {
-        id: 34,
-        name: 'Jean Long Sleeve',
-        imageUrl: 'https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png',
-        price: 40
-      },
-      {
-        id: 35,
-        name: 'Burgundy T-shirt',
-        imageUrl: 'https://i.ibb.co/mh3VM1f/polka-dot-shirt.png',
+        id: 4,
+        name: 'Butterfly',
+        imageUrl: 'https://www.chanel.com/en_GB/fashion/sunglasses/pub/media/catalog/product/cache/image/700x560/af097278c5db4767b0fe9bb92fe21690/s/p/spring20-a71351-x02569-s0116-5420b-c501-s6-pdt_view_1.jpg',
         price: 25
       }
     ]
   }
-];
-
+]
 export default SHOP_DATA;
